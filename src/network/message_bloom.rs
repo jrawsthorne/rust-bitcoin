9e07f5afcabf176279253e399c3e9c8f91268c09
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2020-06-25 23:21:59 +0100

    Bloom filter update flags

--- src/network/message_bloom.rs
+++ src/network/message_bloom.rs
@@ -2,6 +2,10 @@
 //! BIP37 Connection Bloom filtering network messages
 //!
 
+use consensus::encode;
+use consensus::{Decodable, Encodable, ReadExt};
+use std::io;
+
 /// `filterload` message sets the current bloom filter
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct FilterLoad {
@@ -12,10 +16,39 @@ pub struct FilterLoad {
     /// A random value
     pub tweak: u32,
     /// Controls how matched items are added to the filter
-    pub flags: u8,
+    pub flags: BloomFlags,
 }
 
 impl_consensus_encoding!(FilterLoad, filter, hash_funcs, tweak, flags);
+
+/// Bloom filter update flags
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum BloomFlags {
+    /// Never update the filter with outpoints.
+    None,
+    /// Always update the filter with outpoints.
+    All,
+    /// Only update the filter with outpoints if it is P2PK or P2MS
+    PubkeyOnly,
+}
+
+impl Encodable for BloomFlags {
+    fn consensus_encode<W: io::Write>(&self, mut e: W) -> Result<usize, encode::Error> {
+        e.write_all(&[*self as u8])?;
+        Ok(1)
+    }
+}
+
+impl Decodable for BloomFlags {
+    fn consensus_decode<D: io::Read>(mut d: D) -> Result<Self, encode::Error> {
+        Ok(match d.read_u8()? {
+            0 => BloomFlags::None,
+            1 => BloomFlags::All,
+            2 => BloomFlags::PubkeyOnly,
+            _ => return Err(encode::Error::ParseFailed("unknown bloom flag")),
+        })
+    }
+}
 
 /// `filteradd` message updates the current filter with new data
 #[derive(Clone, PartialEq, Eq, Debug)]
